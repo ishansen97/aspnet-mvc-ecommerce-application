4f06ddf8f4bbc7f92c67daaa9797887f98966434
Ishan Seneviratne <ishan.s@calcey.com>
2022-03-14 12:43:43 +0530

    including fluent validation in the project

---
+++ Data/Validation/CinemaValidator.cs
@@ -0,0 +1,20 @@
+﻿using ETicketsStore.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicketsStore.Data.Validation
+{
+    public class CinemaValidator : AbstractValidator<Cinema>
+    {
+        public CinemaValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Logo).NotEmpty().WithMessage("Logo URL is required");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required");
+        }
+    }
+}
